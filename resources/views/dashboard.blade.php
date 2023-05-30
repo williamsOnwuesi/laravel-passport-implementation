a3d84f1566baaf52c4dafaa1d84579ae5537306e
Onwuesi Williams <onwuesiwilliamsemeka@gmail.com>
2023-05-30 12:44:16 +0100

    links and style implementation.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -110,7 +110,8 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Welcome! User</h1>
+                        <!-- <h1 class="mt-4">Good Morning! User</h1> -->
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
