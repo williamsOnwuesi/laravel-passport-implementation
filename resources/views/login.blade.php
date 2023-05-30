a97fbe0df92af27b5773bd6865c48c74e5284722
Onwuesi Williams <onwuesiwilliamsemeka@gmail.com>
2023-05-30 12:26:14 +0100

    links and style implementation.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,29 +27,16 @@
             }
         </style>
 
-        <link href="{{ asset('/view_dependencies/css/styles.css') }}" rel="stylesheet" />
+        <link href="{{ asset('/view_assets/dashboard_assets/css/styles.css') }}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
     </head>
 
     <body style="background: rgba(0, 192, 255)">
-        
-        <h1>Hey Stranger!, Please Login To Your Account.</h1>
-
-        <form action="login" method="POST">
-
-            @csrf
 
             @error('email')
                 <p style="color: red;">{{$message}}</p>
             @enderror
-
-            <input type="text" name = "email" class = "login_input" required/>
-            <input type="password" name = "password" class = "login_input" required/>
-            <button type="submit">Login</button>
-            Dont have an account? <a href="register">Register</a>
-
-        </form>
 
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
@@ -60,13 +47,16 @@
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
                                     <div class="card-body">
-                                        <form>
+                                        <form action="login" method="POST">
+
+                                            @csrf
+
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <input name="email" class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
                                                 <label for="inputEmail">Email address</label>
                                             </div>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                <input name="password" class="form-control" id="inputPassword" type="password" placeholder="Password" />
                                                 <label for="inputPassword">Password</label>
                                             </div>
                                             <div class="form-check mb-3">
@@ -75,12 +65,12 @@
                                             </div>
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
                                                 <a class="small" href="password.html">Forgot Password?</a>
-                                                <a class="btn btn-primary" href="index.html">Login</a>
+                                                <button type="submit" class="btn btn-primary">Login</button>
                                             </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                        <div class="small"><a href="/register">Need an account? Sign up!</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -92,7 +82,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                            <div class="text-muted">Copyright &copy; Paws Creative 2023</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
@@ -104,7 +94,7 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('/view_dependencies/js/scripts.js') }}"></script>
+        <script src="{{ asset('/view_assets/dashboard_assets/js/scripts.js') }}"></script>
 
     </body>
 
